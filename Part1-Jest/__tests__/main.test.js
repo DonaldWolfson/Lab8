e87e01333b109d8121d8e61f90211003bc5dd7d4
DonaldWolfson <dwolfson486@gmail.com>
2021-02-24 16:08:55 -0800

    Fixed Part 1

--- Part1-Jest/__tests__/main.test.js
+++ Part1-Jest/__tests__/main.test.js
@@ -2,19 +2,19 @@ const main = require('../assets/scripts/main');
 
 describe('Main test group', () => {
     test('Icon Level 3 Volume', () => {
-        expect(main.formatVolumeIconPath(67))
+        expect(main(67))
             .toMatch('./assets/media/icons/volume-level-3.svg');
     });
     test('Icon Level 2 Volume', () => {
-        expect(main.formatVolumeIconPath(34))
+        expect(main(34))
             .toMatch('./assets/media/icons/volume-level-2.svg');
     });
     test('Icon Level 1 Volume', () => {
-        expect(main.formatVolumeIconPath(1))
+        expect(main(1))
             .toMatch('./assets/media/icons/volume-level-1.svg');
     });
     test('Icon Level 0 Volume', () => {
-        expect(main.formatVolumeIconPath(0))
+        expect(main(0))
             .toMatch('./assets/media/icons/volume-level-0.svg');
     });
 });
